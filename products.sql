0b809134687227432452d1726d8dd849417670f6
Raquel Betancourt <114664493+edmblue@users.noreply.github.com>
2023-02-05 14:27:11 -0300

    Add files via upload

---
+++ products.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-02-2023 a las 16:57:13
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `products`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products_list`
+--
+
+CREATE TABLE `products_list` (
+  `id` int(11) NOT NULL,
+  `sku` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `size` decimal(10,2) DEFAULT NULL,
+  `height` decimal(10,2) DEFAULT NULL,
+  `width` decimal(10,2) DEFAULT NULL,
+  `length` decimal(10,2) DEFAULT NULL,
+  `weight` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `products_list`
+--
+
+INSERT INTO `products_list` (`id`, `sku`, `name`, `price`, `type`, `size`, `height`, `width`, `length`, `weight`) VALUES
+(51, 'bk-9856', 'Harry Potter Saga', '200.00', 'book', NULL, NULL, NULL, NULL, '12.00'),
+(52, 'frntr-9856', 'Wooden table', '200.00', 'furniture', NULL, '10.00', '20.00', '30.00', NULL),
+(53, 'dvd-9856', 'Rapidos y Furiosos', '20.00', 'dvd', '50.00', NULL, NULL, NULL, NULL),
+(81, 'bk-9810', 'userAdmin', '25.00', 'dvd', '20.00', NULL, NULL, NULL, NULL);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `products_list`
+--
+ALTER TABLE `products_list`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `sku` (`sku`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `products_list`
+--
+ALTER TABLE `products_list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
